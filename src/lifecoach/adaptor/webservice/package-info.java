8302b54861ef1cdbdbcaa978b91099f29994ef25
matteogabburo <matteogabburo@gmail.com>
2017-07-06 10:59:40 +0200

    add maven framework

--- src/lifecoach/adaptor/webservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://webservice.adaptor.lifecoach/")
-package lifecoach.adaptor.webservice;
--- src/lifecoach/localdb/webservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://webservice.localdb.lifecoach/")
-package lifecoach.localdb.webservice;
