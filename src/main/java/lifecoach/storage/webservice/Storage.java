c3eb5732dfe4df9b739fc60f7de73e79dae0806d
matteogabburo <matteogabburo@gmail.com>
2017-07-06 12:26:49 +0200

    Fixed types bug

--- src/main/java/lifecoach/storage/webservice/Storage.java
+++ src/main/java/lifecoach/storage/webservice/Storage.java
@@ -1,7 +1,11 @@
 package lifecoach.storage.webservice;
 
+import ws.localdb.Person;
+import ws.localdb.Measure;
+import ws.localdb.MeasureType;
 import ws.adaptor.Bmi;
-import ws.localdb.*;
+import ws.localdb.Goal;
+import ws.localdb.GoalType;
 
 import java.util.List;
 
@@ -15,122 +19,122 @@ import javax.jws.soap.SOAPBinding.Use;
 
 @WebService
 @SOAPBinding(style = Style.DOCUMENT, use=Use.LITERAL) //optional
-public interface Storage 
+public interface Storage
 {
 	/* Person */
-    
-	@WebMethod(operationName="readPerson")
-    @WebResult(name="person") 
+
+    @WebMethod(operationName="readPerson")
+    @WebResult(name="person")
     public Person readPerson(@WebParam(name="personId") int id);
 
     @WebMethod(operationName="createPerson")
-    @WebResult(name="personId") 
+    @WebResult(name="personId")
     public int addPerson(@WebParam(name="person", targetNamespace="http://webservice.storage.lifecoach/") Person person);
 
     @WebMethod(operationName="updatePerson")
-    @WebResult(name="personId") 
+    @WebResult(name="personId")
     public int updatePerson(@WebParam(name="person", targetNamespace="http://webservice.storage.lifecoach/") Person person);
 
     @WebMethod(operationName="deletePerson")
-    @WebResult(name="result") 
+    @WebResult(name="result")
     public int deletePerson(@WebParam(name="personId") int id);
-    
-    
+
+
     /* Measure */
-    
+
     @WebMethod(operationName="getMeasureHistory")
-    @WebResult(name="measureList") 
+    @WebResult(name="measureList")
     public List<Measure> getMeasures(@WebParam(name="personId") int pId, @WebParam(name="measureTypeId") String typeMeasure);
-    
+
     @WebMethod(operationName="getSomeMeasure")
-    @WebResult(name="measureList") 
+    @WebResult(name="measureList")
     public List<Measure> getSomeMeasures(@WebParam(name="personId") int pId, @WebParam(name="measureTypeId") String typeMeasure, @WebParam(name="nMeasure") int nMeasure);
-    
+
     @WebMethod(operationName="getLastMeasure")
-    @WebResult(name="measureList") 
+    @WebResult(name="measureList")
     public List<Measure> getLastMeasure(@WebParam(name="personId") int pId);
-    
+
     @WebMethod(operationName="getLastMeasureByType")
-    @WebResult(name="measure") 
+    @WebResult(name="measure")
     public Measure getLastMeasureByType(@WebParam(name="personId") int pId, @WebParam(name="measureTypeId") String typeMeasure);
-    
+
     @WebMethod(operationName="readMeasure")
-    @WebResult(name="measure") 
+    @WebResult(name="measure")
     public Measure readMeasure(@WebParam(name="personId") int pId, @WebParam(name="measureTypeId") String typeMeasure, @WebParam(name="measureId") int mId);
-    
+
     @WebMethod(operationName="createMeasure")
-    @WebResult(name="measureId") 
+    @WebResult(name="measureId")
     public int addMeasure(@WebParam(name="personId") int pId, @WebParam(name="measure", targetNamespace="http://webservice.localdb.lifecoach/") Measure measure);
 
     @WebMethod(operationName="updateMeasure")
-    @WebResult(name="measureId") 
+    @WebResult(name="measureId")
     public int updateMeasure(@WebParam(name="personId") int pId, @WebParam(name="measure", targetNamespace="http://webservice.localdb.lifecoach/") Measure measure);
-    
+
     @WebMethod(operationName="deleteMeasure")
-    @WebResult(name="result") 
+    @WebResult(name="result")
     public int deleteMeasure(@WebParam(name="measureId") int id);
-    
-    
+
+
     /* MeasureType */
-    
+
     @WebMethod(operationName="getMeasureTypeList")
-    @WebResult(name="measureTypeList") 
+    @WebResult(name="measureTypeList")
     public List<MeasureType> getMeasureType();
-    
+
     @WebMethod(operationName="readMeasureType")
-    @WebResult(name="measureType") 
+    @WebResult(name="measureType")
     public MeasureType readMeasureType(@WebParam(name="goalId") int id);
-    
+
     @WebMethod(operationName="getIdMeasureTypeByType")
-    @WebResult(name="idMeasureType") 
+    @WebResult(name="idMeasureType")
     public int getIdMeasureTypeByType(@WebParam(name="type") String type);
-    
-    
+
+
     /* Goal */
-    
+
     @WebMethod(operationName="getGoals")
-    @WebResult(name="goalList") 
+    @WebResult(name="goalList")
     public List<Goal> getGoals(@WebParam(name="personId") int pId);
-    
+
     @WebMethod(operationName="getGoalHistory")
-    @WebResult(name="goalList") 
+    @WebResult(name="goalList")
     public List<Goal> getGoal(@WebParam(name="personId") int pId, @WebParam(name="measureTypeId") String typeMeasure);
-    
+
     @WebMethod(operationName="readGoal")
-    @WebResult(name="goal") 
+    @WebResult(name="goal")
     public Goal readGoal(@WebParam(name="personId") int pId, @WebParam(name="measureTypeId") String typeMeasure, @WebParam(name="goalId") int gId);
-    
+
     @WebMethod(operationName="createGoal")
-    @WebResult(name="goalId") 
+    @WebResult(name="goalId")
     public int addGoal(@WebParam(name="personId") int pId, @WebParam(name="goal", targetNamespace="http://webservice.localdb.lifecoach/") Goal goal);
 
     @WebMethod(operationName="updateGoal")
-    @WebResult(name="goalId") 
+    @WebResult(name="goalId")
     public int updateGoal(@WebParam(name="personId") int pId, @WebParam(name="goal", targetNamespace="http://webservice.localdb.lifecoach/") Goal goal);
-    
+
     @WebMethod(operationName="deleteGoal")
-    @WebResult(name="result") 
+    @WebResult(name="result")
     public int deleteGoal(@WebParam(name="goalId") int id);
-    
+
     @WebMethod(operationName="getGoalByTitle")
-    @WebResult(name="goal") 
+    @WebResult(name="goal")
     public Goal getGoalByTitle(@WebParam(name="personId") int pId, @WebParam(name="title") String title);
-    
+
     /* GoalType */
-    
+
     @WebMethod(operationName="getGoalTypeList")
-    @WebResult(name="goalTypeList") 
+    @WebResult(name="goalTypeList")
     public List<GoalType> getGoalType();
-    
+
     @WebMethod(operationName="readGoalType")
-    @WebResult(name="goalType") 
+    @WebResult(name="goalType")
     public GoalType readGoalType(@WebParam(name="goalId") int id);
-    
-    
+
+
     /* Adaptor */
-    
+
     @WebMethod(operationName="getBmi")
-    @WebResult(name="bmi") 
+    @WebResult(name="bmi")
     public Bmi getBmi(@WebParam(name="weight") float weight, @WebParam(name="height") float height,
                       @WebParam(name="sex") char sex, @WebParam(name="age") int age,
                       @WebParam(name="waist") float waist, @WebParam(name="hip") float hip);
--- src/main/java/lifecoach/storage/webservice/StorageImplementation.java
+++ src/main/java/lifecoach/storage/webservice/StorageImplementation.java
@@ -1,42 +1,50 @@
 package lifecoach.storage.webservice;
 
-import ws.adaptor.Adaptor;
+import ws.localdb.PeopleService;
+import ws.localdb.People;
+import ws.localdb.Person;
+import ws.localdb.Measure;
+import ws.localdb.MeasureType;
+import ws.localdb.Goal;
+import ws.localdb.GoalType;
+
 import ws.adaptor.AdaptorService;
+import ws.adaptor.Adaptor;
 import ws.adaptor.Bmi;
-import ws.localdb.*;
 
 import java.util.List;
 
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 //Service Implementation
 
 @WebService(endpointInterface = "lifecoach.storage.webservice.Storage",
-    serviceName="StorageService")
-public class StorageImplementation implements Storage 
+        serviceName="StorageService")
+public class StorageImplementation implements Storage
 {
-	PeopleService service;
-	People people;
-	AdaptorService service_adaptor;
-	Adaptor adaptor;
-	
-	public void init()
-	{
-		service = new PeopleService();
+    PeopleService service;
+    People people;
+    AdaptorService service_adaptor;
+    Adaptor adaptor;
+
+    public void init()
+    {
+        service = new PeopleService();
         people = service.getPeopleImplementationPort();
-	}
-	
-	public void init_adaptor()
-	{
-		service_adaptor = new AdaptorService();
+    }
+
+    public void init_adaptor()
+    {
+        service_adaptor = new AdaptorService();
         adaptor = service_adaptor.getAdaptorImplementationPort();
-	}
-	
+    }
+
 	/* Manage Person*/
-	
+
     @Override
     public Person readPerson(int id) {
-    	init();    	
+        init();
         System.out.println("Reading Person by id = "+id);
         Person p = people.readPerson(id);
         if (p!=null) {
@@ -49,44 +57,44 @@ public class StorageImplementation implements Storage
 
     @Override
     public int addPerson(Person person) {
-    	init();
-    	System.out.println("Save Person with id = " + person.getIdPerson());
-    	return people.createPerson(person);
+        init();
+        System.out.println("Save Person with id = " + person.getIdPerson());
+        return people.createPerson(person);
     }
 
     @Override
     public int updatePerson(Person person) {
-    	init();
-    	if(person == null)
-    	{
-    		System.out.println("Zio billy");
-    		return 1;
-    	}    	
-    	System.out.println("Update Person with id = " + person.getIdPerson());
+        init();
+        if(person == null)
+        {
+            System.out.println("Zio billy");
+            return 1;
+        }
+        System.out.println("Update Person with id = " + person.getIdPerson());
         return people.updatePerson(person);
     }
 
     @Override
     public int deletePerson(int id) {
-    	init();
-    	System.out.println("Delete Person with id = " + id);           
+        init();
+        System.out.println("Delete Person with id = " + id);
         return people.deletePerson(id);
     }
-    
-    
+
+
     /* Manage Measure */
-    
+
     @Override
     public Measure readMeasure(int pId, String measureType, int mId) {
         init();
-    	System.out.println("Reading Measure by pId = " + pId + ", mId = " + mId + ", type = " + measureType);
+        System.out.println("Reading Measure by pId = " + pId + ", mId = " + mId + ", type = " + measureType);
         List<Measure> p = people.getMeasureHistory(pId, measureType);
         if (p!=null) {
             System.out.println("---> Found Measure");
             for(Measure m : p)
             {
-            	System.out.println(m.getIdMeasure() + " " + m.getValue());
-            }	
+                System.out.println(m.getIdMeasure() + " " + m.getValue());
+            }
         } else {
             System.out.println("---> Didn't find any Measure");
         }
@@ -95,59 +103,59 @@ public class StorageImplementation implements Storage
 
     @Override
     public List<Measure> getMeasures(int pId, String measureType) {
-    	init();
-    	System.out.println("Reading Measure by pId = " + pId + ", type = " + measureType);
+        init();
+        System.out.println("Reading Measure by pId = " + pId + ", type = " + measureType);
         List<Measure> p = people.getMeasureHistory(pId, measureType);
         if (p!=null) {
             System.out.println("---> Found Measure");
             for(Measure m : p)
             {
-            	System.out.println(m.getIdMeasure() + " " + m.getValue());
+                System.out.println(m.getIdMeasure() + " " + m.getValue());
             }
         } else {
             System.out.println("---> Didn't find any Measure");
         }
         return p;
     }
-    
+
     @Override
     public List<Measure> getSomeMeasures(int pId, String measureType, int nMeasure) {
-    	init();
-    	System.out.println("Reading Measure by pId = " + pId + ", type = " + measureType);
+        init();
+        System.out.println("Reading Measure by pId = " + pId + ", type = " + measureType);
         List<Measure> p = people.getSomeMeasure(pId, measureType, nMeasure);
         if (p!=null) {
             System.out.println("---> Found Measure");
             for(Measure m : p)
             {
-            	System.out.println(m.getIdMeasure() + " " + m.getValue());
+                System.out.println(m.getIdMeasure() + " " + m.getValue());
             }
         } else {
             System.out.println("---> Didn't find any Measure");
         }
         return p;
     }
-    
+
     @Override
     public List<Measure> getLastMeasure(int pId) {
-    	init();
-    	System.out.println("Reading Last Measure by pId = " + pId);
+        init();
+        System.out.println("Reading Last Measure by pId = " + pId);
         List<Measure> p = people.readLastMeasure(pId);
         if (p!=null) {
             System.out.println("---> Found Measure");
             for(Measure m : p)
             {
-            	System.out.println(m.getIdMeasure() + " " + m.getValue());
+                System.out.println(m.getIdMeasure() + " " + m.getValue());
             }
         } else {
             System.out.println("---> Didn't find any Measure");
         }
         return p;
     }
-    
+
     @Override
     public Measure getLastMeasureByType(int pId, String type) {
-    	init();
-    	System.out.println("Reading Last Measure by pId = " + pId + " and type = " + type);
+        init();
+        System.out.println("Reading Last Measure by pId = " + pId + " and type = " + type);
         Measure p = people.readLastMeasureByType(pId, type);
         if (p!=null) {
             System.out.println("---> Found Measure by id " + p.getIdMeasure());
@@ -159,42 +167,42 @@ public class StorageImplementation implements Storage
 
     @Override
     public int addMeasure(int pId, Measure measure) {
-    	init();
-    	System.out.println("Save Measure with id = " + measure.getIdMeasure());
-        
-    	if(measure.getMeasureType()==null)
-    	{
-    		System.out.println("QUi");
-    	}
-    	else
-    	{
-    		System.out.println("QUa " + measure.getMeasureType().getType());
-    	}
-    	
+        init();
+        System.out.println("Save Measure with id = " + measure.getIdMeasure());
+
+        if(measure.getMeasureType()==null)
+        {
+            System.out.println("QUi");
+        }
+        else
+        {
+            System.out.println("QUa " + measure.getMeasureType().getType());
+        }
+
         return people.createMeasure(pId, measure);
     }
 
     @Override
     public int updateMeasure(int pId, Measure measure) {
-    	init();
-    	System.out.println("Update Measure with id = " + measure.getIdMeasure());  	
-    	return people.updateMeasure(pId, measure);
+        init();
+        System.out.println("Update Measure with id = " + measure.getIdMeasure());
+        return people.updateMeasure(pId, measure);
     }
-    
+
     @Override
     public int deleteMeasure(int id) {
-    	init();
-    	System.out.println("Delete Measure with id = " + id);
-    	return people.deleteMeasure(id);
+        init();
+        System.out.println("Delete Measure with id = " + id);
+        return people.deleteMeasure(id);
     }
-    
-    
+
+
     /* Manage MeasureType */
-    
+
     @Override
     public MeasureType readMeasureType(int id) {
         init();
-    	System.out.println("Reading MeasureType by id = " + id);
+        System.out.println("Reading MeasureType by id = " + id);
         MeasureType m = people.readMeasureType(id);
         if (m!=null) {
             System.out.println("---> Found MeasureType by id = " + id + " => " + m.getType());
@@ -206,25 +214,25 @@ public class StorageImplementation implements Storage
 
     @Override
     public List<MeasureType> getMeasureType() {
-    	init();
-    	System.out.println("Read MeasureType List");
+        init();
+        System.out.println("Read MeasureType List");
         return people.getMeasureTypeList();
     }
-    
+
     @Override
     public int getIdMeasureTypeByType(String type) {
-    	init();
-    	System.out.println("Read MeasureType List");
+        init();
+        System.out.println("Read MeasureType List");
         return people.getIdMeasureTypeByType(type);
     }
-    
-        
+
+
     /* Manage Goal */
-    
+
     @Override
     public List<Goal> getGoals(int pId) {
         init();
-    	System.out.println("Reading Goal by id = " + pId);
+        System.out.println("Reading Goal by id = " + pId);
         List<Goal> m = people.getGoals(pId);
         if (m!=null) {
             System.out.println("---> Found Goal for id = " + pId);
@@ -233,24 +241,24 @@ public class StorageImplementation implements Storage
         }
         return m;
     }
-    
+
     @Override
     public Goal readGoal(int pId, String measureType, int gId) {
         init();
-    	System.out.println("Reading Goal by pId = " + pId + ", mId = " + gId + ", type = " + measureType);
+        System.out.println("Reading Goal by pId = " + pId + ", mId = " + gId + ", type = " + measureType);
         return people.readGoal(pId, measureType, gId);
     }
 
     @Override
     public List<Goal> getGoal(int pId, String measureType) {
-    	init();
-    	System.out.println("Reading Goal by pId = " + pId + ", type = " + measureType);
+        init();
+        System.out.println("Reading Goal by pId = " + pId + ", type = " + measureType);
         List<Goal> p = people.getGoalHistory(pId, measureType);
         if (p!=null) {
             System.out.println("---> Found Goal");
             for(Goal m : p)
             {
-            	System.out.println(m.getIdGoal() + " " + m.getValue());
+                System.out.println(m.getIdGoal() + " " + m.getValue());
             }
         } else {
             System.out.println("---> Didn't find any Goal");
@@ -260,49 +268,49 @@ public class StorageImplementation implements Storage
 
     @Override
     public int addGoal(int pId, Goal goal) {
-    	init();
-    	System.out.println("Save Goal with id = " + goal.getIdGoal());
-        
-    	if(goal.getMeasureType()==null)
-    	{
-    		System.out.println("QUi");
-    	}
-    	else
-    	{
-    		System.out.println("QUa " + goal.getMeasureType().getType());
-    	}
-    	
-    	return people.createGoal(pId, goal);
+        init();
+        System.out.println("Save Goal with id = " + goal.getIdGoal());
+
+        if(goal.getMeasureType()==null)
+        {
+            System.out.println("QUi");
+        }
+        else
+        {
+            System.out.println("QUa " + goal.getMeasureType().getType());
+        }
+
+        return people.createGoal(pId, goal);
     }
 
     @Override
     public int updateGoal(int pId, Goal goal) {
-    	init();
-    	System.out.println("Update Goal with id = " + goal.getIdGoal());
-    	return people.updateGoal(pId, goal);
+        init();
+        System.out.println("Update Goal with id = " + goal.getIdGoal());
+        return people.updateGoal(pId, goal);
     }
-    
+
     @Override
     public int deleteGoal(int id) {
-    	init();
-    	System.out.println("Delete Goal with id = " + id);
-    	return people.deleteGoal(id);
+        init();
+        System.out.println("Delete Goal with id = " + id);
+        return people.deleteGoal(id);
     }
 
-     @Override
+    @Override
     public Goal getGoalByTitle(int pId, String title){
-	init();
-	System.out.println("Goal by id = " + pId + " title = " + title);
+        init();
+        System.out.println("Goal by id = " + pId + " title = " + title);
 
-	return people.getGoalByTitle(pId, title);
+        return people.getGoalByTitle(pId, title);
     }
-    
+
     /* Manage GoalType */
-    
+
     @Override
     public GoalType readGoalType(int id) {
         init();
-    	System.out.println("Reading GoalType by id = " + id);
+        System.out.println("Reading GoalType by id = " + id);
         GoalType m = people.readGoalType(id);
         if (m!=null) {
             System.out.println("---> Found GoalType by id = " + id + " => " + m.getType());
@@ -314,19 +322,19 @@ public class StorageImplementation implements Storage
 
     @Override
     public List<GoalType> getGoalType() {
-    	init();
-    	System.out.println("Read GoalType List");
+        init();
+        System.out.println("Read GoalType List");
         return people.getGoalTypeList();
     }
-    
-    
+
+
     /* Manage Adaptor*/
-    
+
     @Override
     public Bmi getBmi(float weight, float height, char sex, int age, float waist, float hip) {
-    	init_adaptor();    	
-    	System.out.println("Calcolate Bmi with: weight=" + weight + ", height=" + height + ", sex=" + sex
-          		 + ", age=" + age + ", waist=" + waist + ", hip=" + hip);
+        init_adaptor();
+        System.out.println("Calcolate Bmi with: weight=" + weight + ", height=" + height + ", sex=" + sex
+                + ", age=" + age + ", waist=" + waist + ", hip=" + hip);
         Bmi b = adaptor.getBmi(weight, height, sex, age, waist, hip);
         if (b!=null) {
             System.out.println("---> Bmi for = "+weight+" and "+height);
